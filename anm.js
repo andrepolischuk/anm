57f6a457a6d10766a13c4f1f8f17db7ad30450d9
andrepolischuk <andre.polischuk@gmail.com>
2015-01-16 15:50:18 +0300

    Add scaling, opacity and rotation, fix #1 #2 #3

--- anm.js
+++ anm.js
@@ -35,12 +35,24 @@
    * Effects
    */
 
-  var effects = {
-    '-webkit-transform' : 'translate3d',
-       '-moz-transform' : 'translate3d',
-        '-ms-transform' : 'translate3d',
-         '-o-transform' : 'translate',
-            'transform' : 'translate3d'
+  var effects = [
+    'x',
+    'y',
+    'scale',
+    'opacity',
+    'rotate'
+  ];
+
+  /**
+   * Transform map
+   */
+
+  var transformMap = {
+    'webkitTransform' : 'translate3d',
+    'MozTransform' : 'translate3d',
+    'msTransform' : 'translate3d',
+    'OTransform' : 'translate',
+    'transform' : 'translate3d'
   };
 
   /**
@@ -87,6 +99,101 @@
   };
 
   /**
+   * Calculate cursor position
+   * @param  {Object} e
+   * @return {Object}
+   * @api private
+   */
+
+  function calculatePosition(e) {
+    var pos = {};
+    pos.x = e.clientX - window.innerWidth / 2 || 0;
+    pos.y = e.clientY - window.innerHeight / 2 || 0;
+    pos.fi = (Math.atan(pos.x === 0 ? Infinity : -pos.y / pos.x) +
+      (pos.x < 0 ? Math.PI : (-pos.y < 0 ? 2 * Math.PI : 0)));
+    pos.s = 45 * Math.sin(2 * pos.fi) / 100;
+    pos.x /= 100;
+    pos.y /= 100;
+    pos.r = Math.sqrt(Math.pow(pos.x, 2) + Math.pow(pos.y, 2)) /
+      Math.sqrt(Math.pow(window.innerWidth, 2) +
+      Math.pow(window.innerHeight, 2)) * 2;
+    return pos;
+  }
+
+  /**
+   * Calculate element factors
+   * @param  {Object} el
+   * @return {Object}
+   * @api private
+   */
+
+  function calculateFactors(el) {
+    var fact = {};
+    for (var i = 0; i < effects.length; i++) {
+      fact[effects[i]] = +el.getAttribute('data-speed-' + effects[i]) || 0;
+    }
+    return fact;
+  }
+
+  /**
+   * Calculate element transforms
+   * @param  {Object} pos
+   * @param  {Object} fact
+   * @return {Object}
+   * @api private
+   */
+
+  function calculateTransforms(pos, fact) {
+    var transform = {};
+    transform.x = pos.x * fact.x + 'px';
+    transform.y = pos.y * fact.y + 'px';
+    transform.scale = 1 + (pos.r * fact.scale);
+    transform.opacity = 1 - (pos.r * Math.abs(fact.opacity));
+    transform.rotate = -pos.s * pos.r * 100 * fact.rotate + 'deg';
+    return transform;
+  }
+
+  /**
+   * Set element transform styles
+   * @param  {Object} el
+   * @param  {Object} transform
+   * @return {Object}
+   * @api private
+   */
+
+  function setElementStyle(el, transform) {
+
+    if (msie) {
+
+      el.style.marginLeft = transform.x;
+      el.style.marginTop = transform.y;
+
+    } else {
+
+      for (var m in transformMap) {
+        if (transformMap.hasOwnProperty(m)) {
+          el.style[m] = [
+            transformMap[m],
+            '(',
+            transform.x + ',' + transform.y,
+            transformMap[m] === 'translate' ? '' : ',0',
+            ') scale(',
+            transform.scale,
+            ') rotate(',
+            transform.rotate,
+            ')'
+          ].join('');
+        }
+      }
+
+      el.style.opacity = transform.opacity;
+      el.style.MozOpacity = transform.opacity;
+
+    }
+
+  }
+
+  /**
    * Set elements positions
    * @api private
    */
@@ -97,44 +204,16 @@
       return;
     }
 
-    e = e || window.event || {};
+    e = e || {};
     e = e.type === 'touchmove' ? e.changedTouches[0] : e;
 
-    var x = !e ? 0 : (e.clientX - window.innerWidth / 2) * 30 / window.innerWidth;
-    var y = !e ? 0 : (e.clientY - window.innerHeight / 2) * 30 / window.innerHeight;
+    var fact, transform, pos = calculatePosition(e);
 
-    for (var i = 0, offsetX, offsetY, el, string; i < elements.length; i++) {
-
+    for (var i = 0, el; i < elements.length; i++) {
       el = elements[i];
-
-      // calculate offsets
-      offsetX = x * (+el.getAttribute('data-speed-x') || 0) + 'px';
-      offsetY = y * (+el.getAttribute('data-speed-y') || 0) + 'px';
-
-      // set style
-      if (msie) {
-
-        el.style.marginLeft = offsetX;
-        el.style.marginTop  = offsetY;
-
-      } else {
-
-        string = [offsetX, offsetY].join(',');
-
-        for (var f in effects) {
-          if (effects.hasOwnProperty(f)) {
-            el.style[f] = [
-              effects[f],
-              '(',
-              string,
-              effects[f] === 'translate' ? '' : ',0',
-              ')'
-            ].join('');
-          }
-        }
-
-      }
-
+      fact = calculateFactors(el);
+      transform = calculateTransforms(pos, fact);
+      setElementStyle(el, transform);
     }
 
   }
